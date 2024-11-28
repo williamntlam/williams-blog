66a8bf937d4c467b2ae634b226e7bf207cb70e7b
William Nhut Lam <williamntlam@gmail.com>
2024-11-28 00:45:44 -0500

    feat: created page for csc309 e1

---
+++ pages/csc309-e11.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div id="auto-tester">
+      Hi. I am <strong>lamwill7</strong>, and I have learned how to develop a
+      full-stack web app and deploy it!
+    </div>
+  );
+};
+
+export default Home;
